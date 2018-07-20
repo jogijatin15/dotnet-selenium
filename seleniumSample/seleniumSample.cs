2c6b53ee3ca9cd1d689e6039894cb297ccd7d968
Jogi, Jatin <>
2018-07-19 22:02:43 -0400

    grid execution

--- seleniumSample/seleniumSample.cs
+++ seleniumSample/seleniumSample.cs
@@ -7,25 +7,55 @@ using NUnit.Framework;
 using OpenQA.Selenium;
 using OpenQA.Selenium.Chrome;
 using OpenQA.Selenium.Firefox;
-
+using OpenQA.Selenium.Remote;
 
 namespace ClassLibrary1
 {
     public class openChrome
-
     {
+        private IWebDriver driver;
         [Test]
         public void testSeNew()
         {
             Console.WriteLine("Hello World!");
-            IWebDriver driver = new ChromeDriver();
+
+            DesiredCapabilities caps = new DesiredCapabilities();
+            caps.SetCapability(CapabilityType.BrowserName, "chrome");
+            caps.SetCapability(CapabilityType.Version, "latest");
+            caps.SetCapability(CapabilityType.Platform, "Windows 10");
+            
+            caps.SetCapability("username", "");
+            caps.SetCapability("accessKey", "");
+            caps.SetCapability("name", "Test Case 1");
+             
+            //IWebDriver driver;
+            driver = new RemoteWebDriver(new Uri("http://ondemand.saucelabs.com:80/wd/hub"), caps, TimeSpan.FromSeconds(600));
+            //IWebDriver driver = new ChromeDriver();
             driver.Navigate().GoToUrl("http://www.google.com");
             //StringAssert.Contains("Google", driver.Title);
             IWebElement query = driver.FindElement(By.Name("q"));
             query.SendKeys("Sauce Labs");
             query.Submit();
             driver.Close();
-            driver.Quit();
+            //driver.Quit();
         }
+
+        [TearDown]
+        public void CleanUp()
+        {
+            bool passed = TestContext.CurrentContext.Result.Outcome.Status == NUnit.Framework.Interfaces.TestStatus.Passed;
+            try
+            {
+                // Logs the result to Sauce Labs
+                ((IJavaScriptExecutor)driver).ExecuteScript("sauce:job-result=" + (passed ? "passed" : "failed"));
+            }
+            finally
+            {
+                // Terminates the remote webdriver session
+                driver.Quit();
+            }
+        }
+
+
     }
 }
